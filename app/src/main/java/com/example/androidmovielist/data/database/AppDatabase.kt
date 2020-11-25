4861ae3418b03f7c0931a804142ede86a1b79181
David Lopez Dayer <davidlopez.dayer@dnata.com>
2020-11-25 10:12:13 +0400

    Add dagger dependencies

--- app/src/main/java/com/example/androidmovielist/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/androidmovielist/data/database/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.example.androidmovielist.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [LocalMovie::class], version = 1)
+@Database(entities = [LocalMovie::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun movieDao(): MovieDao
 }
